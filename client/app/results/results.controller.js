a826d992718db186750b43eded5195072cd41337
devmeyster <arthurfbi@yahoo.com>
2015-03-26 16:05:12 -0700

    added a like/dislike view for restaurant discovery

--- client/app/results/results.controller.js
+++ client/app/results/results.controller.js
@@ -10,17 +10,41 @@ app.controller('ResultsCtrl', function ($scope,CheckLoggedIn, $location, SearchR
       }
     });
   };
-  $scope.restaurants = SharedData.get('results'); // get the results from the data store.
 
-  $scope.restaurants.forEach(function(restaurant){
-    GetRating(restaurant.id)
-    .success(function(data,status,headers,config){
-      restaurant.avgRating = data.avgRating;
-    })
-    .error(function(data,status,headers,config){
-      console.error('Error: ' + data);
-    })
-  });
+  $scope.restaurants = [
+    { id: 'gary-danko-san-francisco', name: 'Gary Danko', users: ['arianf', 'devmeyster', 'rob'],         rating: 4.5, photo_url:"http://hamelfamilywines.com/wp-content/uploads/2013/01/Gary-Danko1.jpg" },
+    { id: 'chipotle-san-francisco',   name: 'Chipotle',   users: ['knatsuki92', 'ThornWinters'],          rating: 3,   photo_url:"http://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1024px-Chipotle_Mexican_Grill_logo.svg.png" },
+    { id: 'mc-donalds-san-francisco', name: 'McDonalds',  users: ['gistrict9', 'asadsheikh', 'bportnoy'], rating: 3.5, photo_url:"http://img1.wikia.nocookie.net/__cb20100717060808/logopedia/images/a/a9/Mcdonalds-90s-logo.svg" },
+    { id: 'carl-jr-san-francisco',    name: 'Carl Jr',    users: ['gistrict9', 'asadsheikh', 'bportnoy'], rating: 2.5, photo_url:"https://fastfoodmenuprice.com/wp-content/uploads/2014/11/carls.jpeg"},
+    { id: 'subway-san-francisco',     name: 'Subway',     users: ['gistrict9', 'asadsheikh', 'bportnoy'], rating: 3.5, photo_url:"http://fontmeme.com/images/Subway-Logo.jpg" }
+  ];
+  
+  $scope.dislike = function (restaurant){
+    console.log("disliked", restaurant);
+    $scope.restaurants.shift();
+  };
+  $scope.like = function (restaurant){
+    console.log("disliked", restaurant);
+    if($scope.restaurants.length-1 === 0){
+      alert("no more restaurants in the queue");
+    }
+    $scope.restaurants.shift();
+  };
+
+  $scope.go = function ( path ) {
+    $location.path( path );
+  };
+
+  $scope.addPlace = function (restaurant){
+    console.log($scope);
+    var newLoc = {};
+    newLoc.id = restaurant.id;
+    newLoc.name = restaurant.name;
+    newLoc.rating = 4.3;
+    console.log(newLoc);
+    SearchRestaurants.addLocation(newLoc);
+    $scope.results = null;
+  };
 
   $scope.oneAtATime = true;
   
