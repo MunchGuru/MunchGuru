ae2c9251453ed9bc31ea15c1c5bd12dad197f3f8
Kuni Natsuki <knatsuki92@gmail.com>
2015-03-26 15:11:13 -0700

    working on sendvote API

---
+++ server/api/sendvote/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./sendvote.controller');
+
+var router = express.Router();
+
+router.post('/', controller.sendvote.addVote);
+
+module.exports = router;
