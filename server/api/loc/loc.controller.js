19cd1b6dc54c94a3838befabde8073a17944ed3f
Kuni Natsuki <knatsuki92@gmail.com>
2015-03-27 16:20:23 -0700

    backend API cleaned and added response for missing data for GET /api/loc

--- server/api/loc/loc.controller.js
+++ server/api/loc/loc.controller.js
@@ -13,12 +13,6 @@ exports.loc = {
     var img_url = req.body.restaurant.image_url;
     var org_id = req.body.restaurant.org_id;
 
-    console.log(yelp_id);
-    console.log(name);
-    console.log(rating);
-    console.log(img_url);
-    console.log(org_id);
-
     new Location({yelp_id: yelp_id, organization_id: org_id}).fetch().then(function(found){
       if(found) {
         res.status(200).send('Location already posted');
@@ -26,26 +20,20 @@ exports.loc = {
         new Location({yelp_id: yelp_id, organization_id: org_id, name: name, rating: rating, img_url: img_url}).save().then(function(model){
         res.status(200).send('Location successfully posted');
         console.log('Location successfully posted');
-        console.log("model", model);
+        console.log("location", model);
         });
       }
     });
 },
   getLoc: function(req,res) {
-
     var org_id = req.params.orgId;
-    console.log('orgId', org_id);
-
     var locArray=[];
-
     var LocItem = function(yelp_id, locName, users, rating){
       this.id = yelp_id;
       this.name = locName;
       this.users = users;
       this.rating = rating;
     };
-
-
 
     new Location().query('where', 'organization_id', '=', org_id).fetchAll().then(function(collection){
 
@@ -56,7 +44,9 @@ exports.loc = {
           var locName = model.get('name');
           var rating = model.get('rating');
 
-          console.log('MODEL:', locId, locName, rating);
+          if(!locId || !locName || !rating){
+            res.send(404);
+          }
 
           new Vote().query('where', 'location_id', '=', locId).fetchAll().then(function(user_coll){
             // console.log('user:', user_coll);
@@ -64,10 +54,8 @@ exports.loc = {
             console.log('users: ', users);
             user_coll.forEach(function(model){
               users.push(model.get('user_info'));
-              console.log(model.get('user_info'));
             });
             var newLocItem = new LocItem(locId, locName, users, rating);
-            // console.log(newLocItem);
             locArray.push(newLocItem);
             count++
             if(count === total){
