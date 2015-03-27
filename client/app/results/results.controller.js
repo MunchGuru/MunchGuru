b2b00e5f3b324add5d2d5801e3f3c65a35e34cae
devmeyster <arthurfbi@yahoo.com>
2015-03-27 10:20:05 -0700

    disabled search autocomplete

--- client/app/results/results.controller.js
+++ client/app/results/results.controller.js
@@ -11,14 +11,16 @@ app.controller('ResultsCtrl', function ($scope,CheckLoggedIn, $location, SearchR
     });
   };
 
-  $scope.restaurants = [
+  $scope.restaurants = SearchRestaurants.getLocations();
+/*
+  [
     { id: 'gary-danko-san-francisco', name: 'Gary Danko', users: ['arianf', 'devmeyster', 'rob'],         rating: 4.5, photo_url:"http://www.hothangups.com/p7lsm_img_1/thumbs/GaryDanko_tmb.jpg" },
     { id: 'chipotle-san-francisco',   name: 'Chipotle',   users: ['knatsuki92', 'ThornWinters'],          rating: 3,   photo_url:"http://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1024px-Chipotle_Mexican_Grill_logo.svg.png" },
     { id: 'mc-donalds-san-francisco', name: 'McDonalds',  users: ['gistrict9', 'asadsheikh', 'bportnoy'], rating: 3.5, photo_url:"http://img1.wikia.nocookie.net/__cb20100717060808/logopedia/images/a/a9/Mcdonalds-90s-logo.svg" },
     { id: 'carl-jr-san-francisco',    name: 'Carl Jr',    users: ['gistrict9', 'asadsheikh', 'bportnoy'], rating: 2.5, photo_url:"https://fastfoodmenuprice.com/wp-content/uploads/2014/11/carls.jpeg"},
     { id: 'subway-san-francisco',     name: 'Subway',     users: ['gistrict9', 'asadsheikh', 'bportnoy'], rating: 3.5, photo_url:"http://fontmeme.com/images/Subway-Logo.jpg" }
   ];
-  
+*/
   $scope.dislike = function (restaurant){
     console.log("disliked", restaurant);
     if($scope.restaurants.length === 1){
